6d411b21b38c01f8be7aed68e94319987a6eb214
= <ganesh.deshmukh@haptik.ai>
2021-12-18 07:24:45 +0530

    Registered the App, model is added into db, in website.models.

--- ecom/website/models.py
+++ ecom/website/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=40)
-    price = models.IntegerField(max_length=5)
+    price = models.IntegerField()
     description = models.TextField(max_length=100)
     image_url = models.CharField(max_length=5000, null=True, blank=True)
     category = models.CharField(max_length=12)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
-    class Meta:
-        app_label = 'products'
+    # class Meta:
+    #     app_label = 'website'

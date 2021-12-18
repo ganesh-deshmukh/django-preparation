cfd20d121a06dfc07ba4e4293494910b31db6cc9
= <ganesh.deshmukh@haptik.ai>
2021-12-18 07:05:07 +0530

    python3 manage.py startapp website

--- ecom/website/models.py
+++ ecom/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.IntegerField(max_length=5)
+    description = models.TextField(max_length=100)
+    image_url = models.CharField(max_length=5000, null=True, blank=True)
+    category = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        app_label = 'products'
--- ecom/website/views.py
+++ ecom/website/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+# from django.http.response import HttpResponse, JsonResponse
+from .models import Product
 
-# Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    data = {
+        "products": products
+    }
+    return render(request, 'index.html', context=data)
+
+
+def login(request):
+    products = Product.objects.all()
+    data = {
+        "products": products
+    }
+    return render(request, 'login.html', context=data)
+
+def about(request):
+    # return HttpResponse("About Us page.")
+    return render(request, 'about.html', context={})
+
+
+def contact(request):
+    # return HttpResponse("Contact Us page.")
+    return render(request, 'contact.html', context={})
+
+
+def services(request):
+    # return HttpResponse("services Us page.")
+    return render(request, 'services.html', context={})
+
+
+def json_resp(request):
+    return JsonResponse({"success": True, "message_info": "API Response in JSON."})
